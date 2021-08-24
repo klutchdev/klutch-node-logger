b54a25e98f482d21a7d2b18102b2e0292c3266b5
Kyle Leary (KlutchDEV) <klutchdevelopment@gmail.com>
2021-08-24 07:00:39 -0400

    Change module type to es6

--- index.js
+++ index.js
@@ -1,93 +1,72 @@
-var __importDefault = (undefined && undefined.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.parseJson = exports.stringify = exports.infoIconLog = exports.errorIconLog = exports.warnIconLog = exports.successIconLog = exports.infoLog = exports.errorLog = exports.warnLog = exports.successLog = exports.clear = exports.timeLog = exports.timeEnd = exports.time = exports.table = exports.info = exports.error = exports.warn = exports.log = void 0;
-var log_symbols_1 = __importDefault(require("log-symbols"));
-var chalk_1 = __importDefault(require("chalk"));
+import logSymbols from 'log-symbols';
+import chalk from 'chalk';
+
 // Colors
-var green = chalk_1.default.green;
-var yellow = chalk_1.default.yellow;
-var red = chalk_1.default.red;
-var blue = chalk_1.default.blueBright;
+const green = chalk.green;
+const yellow = chalk.yellow;
+const red = chalk.red;
+const blue = chalk.blueBright;
 // Icons
-var iconSuccess = log_symbols_1.default.success;
-var iconWarn = log_symbols_1.default.warning;
-var iconError = log_symbols_1.default.error;
-var iconInfo = log_symbols_1.default.info;
+const iconSuccess = logSymbols.success;
+const iconWarn = logSymbols.warning;
+const iconError = logSymbols.error;
+const iconInfo = logSymbols.info;
 function log(msg) {
     console.log(msg);
 }
-exports.log = log;
 function warn(msg) {
     console.warn(msg);
 }
-exports.warn = warn;
 function error(msg) {
     console.error(msg);
 }
-exports.error = error;
 function info(msg) {
     console.info(msg);
 }
-exports.info = info;
 function table(obj) {
     console.table(obj);
 }
-exports.table = table;
 function time(label) {
     console.time(label);
 }
-exports.time = time;
 function timeEnd(label) {
     console.timeEnd(label);
 }
-exports.timeEnd = timeEnd;
 function timeLog(label, operation) {
     console.timeLog(label, operation);
 }
-exports.timeLog = timeLog;
 function clear() {
     console.clear();
 }
-exports.clear = clear;
 function successLog(msg) {
     log(green(msg));
 }
-exports.successLog = successLog;
 function warnLog(msg) {
     warn(yellow(msg));
 }
-exports.warnLog = warnLog;
 function errorLog(msg) {
     error(red(msg));
 }
-exports.errorLog = errorLog;
 function infoLog(msg) {
     info(blue(msg));
 }
-exports.infoLog = infoLog;
 function successIconLog(msg) {
     log(green(iconSuccess, msg));
 }
-exports.successIconLog = successIconLog;
 function warnIconLog(msg) {
     warn(yellow(iconWarn, msg));
 }
-exports.warnIconLog = warnIconLog;
 function errorIconLog(msg) {
     error(red(iconError, msg));
 }
-exports.errorIconLog = errorIconLog;
 function infoIconLog(msg) {
     info(blue(iconInfo, msg));
 }
-exports.infoIconLog = infoIconLog;
 function stringify(obj) {
     log(JSON.stringify(obj, null, 2));
 }
-exports.stringify = stringify;
 function parseJson(str) {
     log(JSON.parse(str));
 }
-exports.parseJson = parseJson;
+
+export { clear, error, errorIconLog, errorLog, info, infoIconLog, infoLog, log, parseJson, stringify, successIconLog, successLog, table, time, timeEnd, timeLog, warn, warnIconLog, warnLog };
